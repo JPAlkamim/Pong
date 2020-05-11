8dc2fe0ccfb51acd84554ce060c867ccb9b8a7f3
JpAlkamim <ra112648@uem.br>
2020-05-11 20:06:31 -0300

    first things

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour {
+
+    public Player player1;
+    public Player player2;
+    public Text score1;
+    public Text score2;
+
+    private int _1 = 0;
+    private int _2 = 0;
+
+    public Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start () {
+        body = GetComponent<Rigidbody2D> ();
+        body.velocity = new Vector2 (3, Random.Range (-3, 3));
+    }
+
+    // Update is called once per frame
+    void Update () {
+        body.velocity = new Vector2 (Mathf.Sign (body.velocity.x) * 3, Mathf.Sign (body.velocity.y) * 2);
+
+        if (transform.position.x < player1.transform.position.x) {
+            score2.text = _2++.ToString ();
+
+            transform.position = new Vector2 (0, 0);
+        }
+
+        if (transform.position.x > player2.transform.position.x) {
+            score1.text = _1++.ToString ();
+
+            transform.position = new Vector2 (0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public KeyCode UP;
+    public KeyCode DOWN;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey (UP)) {
+            GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, speed);
+        } else if (Input.GetKey (DOWN)) {
+            GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, -speed);
+        } else {
+            GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+        }
+
+    }
+}
